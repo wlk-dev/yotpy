ecfcf0d60946ee1977171ae4f46275eb269b9d8a
= <william.koelling@gmail.com>
2023-04-20 12:47:39 -0500

    this is why I shouldn't work on this on an empty stomach, just read the docs ffs

--- yotpy/__init__.py
+++ yotpy/__init__.py
@@ -2,7 +2,7 @@
 Yotpy: An easy-to-use Python wrapper for the Yotpo web API.
 """
 
-__version__ = "0.0.82"
+__version__ = "0.0.83"
 
 from .core import YotpoAPIWrapper, JSONTransformer
 from .exceptions import CustomException, SessionNotCreatedError, FailedToGetTokenError, PreflightException, UploadException, SendException, UserNotFound, AppNotFound
