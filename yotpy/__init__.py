d2dd7a4153795116c04f261c5d2cad16b73da977
= <william.koelling@gmail.com>
2023-06-05 12:54:11 -0500

    updated requirements

--- yotpy/__init__.py
+++ yotpy/__init__.py
@@ -2,7 +2,7 @@
 Yotpy: An easy-to-use Python wrapper for the Yotpo web API.
 """
 
-__version__ = "0.0.86"
+__version__ = "0.0.87"
 
 from .core import YotpoAPIWrapper, JSONTransformer
 from .exceptions import CustomException, SessionNotCreatedError, FailedToGetTokenError, PreflightException, UploadException, SendException, UserNotFound, AppNotFound
